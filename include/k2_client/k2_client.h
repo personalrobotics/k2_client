bcf510950066c1c002d616417288a09040fc67fd
Pras Velagapudi <psigen@gmail.com>
2016-03-19 20:42:03 -0400

    Started massive refactor to clean up all of this client code.

--- include/k2_client/k2_client.h
+++ include/k2_client/k2_client.h
@@ -25,77 +25,26 @@ BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY 
 WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***************************************************************************************/
-#ifndef _startKinect_H_
-#define _startKinect_H_
+#ifndef K2_CLIENT_H
+#define K2_CLIENT_H
 
-//*********************************System headers**********************************//
-#include <iostream>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
+#include <boost/asio.hpp>
 
-//*******************************ROS specific Headers******************************//
-#include "ros/ros.h"
-
-//***********************************Other Headers*********************************//
-#include "cv_bridge/cv_bridge.h"
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include "image_transport/image_transport.h"
-#include "sensor_msgs/Image.h"
-#include "sensor_msgs/image_encodings.h"
-#include <json/json.h>
-#include <camera_info_manager/camera_info_manager.h>
-
-class Socket
+namespace k2_client
 {
-public:
-	struct sockaddr_in inAddress;				//IP and port container for easy readability
-	struct sockaddr socketAddress;				//IP and port container for the code
-	struct addrinfo hints, *response, *iterator;//hint specifies what options to look for
-												//response is a linked list that getaddrinfo fills for us
-												//iterator is later on used to go through each of the response
-	int mSocket;
-	int mBufferSize;
-	char *mBuffer;
-	Socket(const char address[], const char portNum[], int bufferSize);
-	void readData();
-private:
 
-};
-
-
-Socket::Socket(const char address[], const char portNum[], int bufferSize)
+/**
+ * Convert a boost::asio buffer to a std::string.
+ */
+std::string asioBufferToString(const boost::asio::streambuf &buffer)
 {
-	memset(&(this->hints), 0, sizeof (this->hints));
-	this->hints.ai_family = AF_INET;
-	this->hints.ai_socktype = SOCK_STREAM;
-	this->mBufferSize = bufferSize;
-	this->mBuffer = new char[this->mBufferSize];
-	getaddrinfo(address,portNum,&(this->hints),&(this->response));
-	for(this->iterator = this->response;this->iterator!=NULL;this->iterator = this->iterator->ai_next)
-	{
-		if(iterator->ai_family != AF_INET)
-		{
-			std::cout<<"Please disable IPv6"<<std::endl;
-			return;
-		}
-		struct sockaddr_in *address = (struct sockaddr_in *)(iterator->ai_addr);
-		char ipString[INET_ADDRSTRLEN];
-		inet_ntop(iterator->ai_family,&(address->sin_addr),ipString,sizeof ipString);
-		this->mSocket = socket(this->response->ai_family,this->response->ai_socktype,this->response->ai_protocol);
-		if(this->mSocket !=-1)
-		connect(this->mSocket,this->response->ai_addr,this->response->ai_addrlen);
-	}
+  using boost::asio::buffers_begin;
+
+  boost::asio::streambuf::const_buffers_type bufs = buffer.data();
+  std::string result(buffers_begin(bufs), buffers_begin(bufs) + buffer.size());
+  return result;
 }
 
-void Socket::readData()
-{
-	memset(this->mBuffer,0,this->mBufferSize);
-	recv(this->mSocket,this->mBuffer,this->mBufferSize,MSG_WAITALL);
-}
+} // namespace k2_client
 
-#endif
+#endif // K2_CLIENT_H
