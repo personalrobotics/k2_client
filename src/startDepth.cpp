b3a251728a7b4eb3f18828669cca22d3a68a7ada
Guo JiaCheng <gjc13@users.noreply.github.com>
2016-05-03 15:09:59 +0800

    Change to flip x axis

--- src/startDepth.cpp
+++ src/startDepth.cpp
@@ -37,27 +37,27 @@ std::string cameraFrame = "";
 
 int main(int argC,char **argV)
 {
-	ros::init(argC,argV,"startDepth");
-	ros::NodeHandle n(cameraName);
-	image_transport::ImageTransport imT(n);
-	std::string serverAddress;
-	n.getParam("/serverNameOrIP",serverAddress);
+    ros::init(argC,argV,"startDepth");
+    ros::NodeHandle n(cameraName);
+    image_transport::ImageTransport imT(n);
+    std::string serverAddress;
+    n.getParam("/serverNameOrIP",serverAddress);
     n.getParam(ros::this_node::getNamespace().substr(1,std::string::npos) +
             "/depth_frame", cameraFrame);
-	Socket mySocket(serverAddress.c_str(), "18000", streamSize);
+    Socket mySocket(serverAddress.c_str(), "18000", streamSize);
     image_transport::Publisher imagePublisher = imT.advertise(imageTopicSubName, 1);
-	cv::Mat frame;
-	cv_bridge::CvImage cvImage;
-	sensor_msgs::Image rosImage;
-	while(ros::ok())
-	{
-		mySocket.readData();
-//        for(int i=0; i < 6; i++) {
-//            printf("%02x", (unsigned char)(mySocket.mBuffer[(1920 * 1079 + 1000) * 6 + i]));
-//        }
-//        printf("\n");
+    cv::Mat frame;
+    cv_bridge::CvImage cvImage;
+    sensor_msgs::Image rosImage;
+    while(ros::ok())
+    {
+        mySocket.readData();
+//      for(int i=0; i < 6; i++) {
+//          printf("%02x", (unsigned char)(mySocket.mBuffer[(1920 * 1079 + 1000) * 6 + i]));
+//      }
+//      printf("\n");
         frame = cv::Mat(cv::Size(1920,1080), CV_16SC3,mySocket.mBuffer);
-		cv::flip(frame,frame,1);
+		cv::flip(frame,frame,0);
         cv::Vec3s test_vec = frame.at<cv::Vec3s>(500, 500);
         cvImage.header.frame_id = cameraFrame.c_str();
 		cvImage.encoding = "16SC3";
@@ -65,6 +65,6 @@ int main(int argC,char **argV)
 		cvImage.toImageMsg(rosImage);
         imagePublisher.publish(rosImage);
 		ros::spinOnce();
-	}
-	return 0;
+    }
+    return 0;
 }
