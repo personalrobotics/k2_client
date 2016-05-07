47d9f91680710bd42974069bcc3a9fd5f4cf579a
sugar10w <sugar10w@163.com>
2016-05-07 22:06:00 +0800

    [debugging] startFace

--- src/startFace.cpp
+++ src/startFace.cpp
@@ -46,45 +46,45 @@ int main(int argC,char **argV)
                 face.expressions.neutral = jsonObject[i]["FaceProperties"]["Neutral"].asBool();
                 face.expressions.neutral = jsonObject[i]["FaceProperties"]["Happy"].asBool();
                 
-                face.facePointsInInfraredSpace.eyeLeftX = jsonObject[i]["FacePointsInInfraredSpace"]["EyeLeft"]["X"];
-                face.facePointsInInfraredSpace.eyeLeftY = jsonObject[i]["FacePointsInInfraredSpace"]["EyeLeft"]["Y"];
-                face.facePointsInInfraredSpace.eyeRightX = jsonObject[i]["FacePointsInInfraredSpace"]["EyeRight"]["X"];
-                face.facePointsInInfraredSpace.eyeRightY = jsonObject[i]["FacePointsInInfraredSpace"]["EyeRight"]["Y"];
-                face.facePointsInInfraredSpace.noseX = jsonObject[i]["FacePointsInInfraredSpace"]["EyeRight"]["X"];
-                face.facePointsInInfraredSpace.noseY = jsonObject[i]["FacePointsInInfraredSpace"]["EyeRight"]["Y"];
-                face.facePointsInInfraredSpace.mouthCornerLeftX = jsonObject[i]["FacePointsInInfraredSpace"]["MouthCornerLeft"]["X"];
-                face.facePointsInInfraredSpace.mouthCornerLeftY = jsonObject[i]["FacePointsInInfraredSpace"]["MouthCornerLeft"]["Y"];
-                face.facePointsInInfraredSpace.mouthCornerRightX = jsonObject[i]["FacePointsInInfraredSpace"]["MouthCornerRight"]["X"];
-                face.facePointsInInfraredSpace.mouthCornerRightY = jsonObject[i]["FacePointsInInfraredSpace"]["MouthCornerRight"]["Y"];
+                face.facePointsInInfraredSpace.eyeLeftX = jsonObject[i]["FacePointsInInfraredSpace"]["EyeLeft"]["X"].asDouble();
+                face.facePointsInInfraredSpace.eyeLeftY = jsonObject[i]["FacePointsInInfraredSpace"]["EyeLeft"]["Y"].asDouble();
+                face.facePointsInInfraredSpace.eyeRightX = jsonObject[i]["FacePointsInInfraredSpace"]["EyeRight"]["X"].asDouble();
+                face.facePointsInInfraredSpace.eyeRightY = jsonObject[i]["FacePointsInInfraredSpace"]["EyeRight"]["Y"].asDouble();
+                face.facePointsInInfraredSpace.noseX = jsonObject[i]["FacePointsInInfraredSpace"]["EyeRight"]["X"].asDouble();
+                face.facePointsInInfraredSpace.noseY = jsonObject[i]["FacePointsInInfraredSpace"]["EyeRight"]["Y"].asDouble();
+                face.facePointsInInfraredSpace.mouthCornerLeftX = jsonObject[i]["FacePointsInInfraredSpace"]["MouthCornerLeft"]["X"].asDouble();
+                face.facePointsInInfraredSpace.mouthCornerLeftY = jsonObject[i]["FacePointsInInfraredSpace"]["MouthCornerLeft"]["Y"].asDouble();
+                face.facePointsInInfraredSpace.mouthCornerRightX = jsonObject[i]["FacePointsInInfraredSpace"]["MouthCornerRight"]["X"].asDouble();
+                face.facePointsInInfraredSpace.mouthCornerRightY = jsonObject[i]["FacePointsInInfraredSpace"]["MouthCornerRight"]["Y"].asDouble();
                 
-                face.facePointsInColorSpace.eyeLeftX = jsonObject[i]["FacePointsInColorSpace"]["EyeLeft"]["X"];
-                face.facePointsInColorSpace.eyeLeftY = jsonObject[i]["FacePointsInColorSpace"]["EyeLeft"]["Y"];
-                face.facePointsInColorSpace.eyeRightX = jsonObject[i]["FacePointsInColorSpace"]["EyeRight"]["X"];
-                face.facePointsInColorSpace.eyeRightY = jsonObject[i]["FacePointsInColorSpace"]["EyeRight"]["Y"];
-                face.facePointsInColorSpace.noseX = jsonObject[i]["FacePointsInColorSpace"]["EyeRight"]["X"];
-                face.facePointsInColorSpace.noseY = jsonObject[i]["FacePointsInColorSpace"]["EyeRight"]["Y"];
-                face.facePointsInColorSpace.mouthCornerLeftX = jsonObject[i]["FacePointsInColorSpace"]["MouthCornerLeft"]["X"];
-                face.facePointsInColorSpace.mouthCornerLeftY = jsonObject[i]["FacePointsInColorSpace"]["MouthCornerLeft"]["Y"];
-                face.facePointsInColorSpace.mouthCornerRightX = jsonObject[i]["FacePointsInColorSpace"]["MouthCornerRight"]["X"];
-                face.facePointsInColorSpace.mouthCornerRightY = jsonObject[i]["FacePointsInColorSpace"]["MouthCornerRight"]["Y"];
+                face.facePointsInColorSpace.eyeLeftX = jsonObject[i]["FacePointsInColorSpace"]["EyeLeft"]["X"].asDouble();
+                face.facePointsInColorSpace.eyeLeftY = jsonObject[i]["FacePointsInColorSpace"]["EyeLeft"]["Y"].asDouble();
+                face.facePointsInColorSpace.eyeRightX = jsonObject[i]["FacePointsInColorSpace"]["EyeRight"]["X"].asDouble();
+                face.facePointsInColorSpace.eyeRightY = jsonObject[i]["FacePointsInColorSpace"]["EyeRight"]["Y"].asDouble();
+                face.facePointsInColorSpace.noseX = jsonObject[i]["FacePointsInColorSpace"]["EyeRight"]["X"].asDouble();
+                face.facePointsInColorSpace.noseY = jsonObject[i]["FacePointsInColorSpace"]["EyeRight"]["Y"].asDouble();
+                face.facePointsInColorSpace.mouthCornerLeftX = jsonObject[i]["FacePointsInColorSpace"]["MouthCornerLeft"]["X"].asDouble();
+                face.facePointsInColorSpace.mouthCornerLeftY = jsonObject[i]["FacePointsInColorSpace"]["MouthCornerLeft"]["Y"].asDouble();
+                face.facePointsInColorSpace.mouthCornerRightX = jsonObject[i]["FacePointsInColorSpace"]["MouthCornerRight"]["X"].asDouble();
+                face.facePointsInColorSpace.mouthCornerRightY = jsonObject[i]["FacePointsInColorSpace"]["MouthCornerRight"]["Y"].asDouble();
                 
-                face.faceBoundingBoxInInfraredSpace.left   = jsonObject[i]["FaceBoundingBoxInInfraredSpace"]["Left"];
-                face.faceBoundingBoxInInfraredSpace.top    = jsonObject[i]["FaceBoundingBoxInInfraredSpace"]["Top"];
-                face.faceBoundingBoxInInfraredSpace.right  = jsonObject[i]["FaceBoundingBoxInInfraredSpace"]["Right"];
-                face.faceBoundingBoxInInfraredSpace.bottom = jsonObject[i]["FaceBoundingBoxInInfraredSpace"]["Bottom"];
+                face.faceBoundingBoxInInfraredSpace.left   = jsonObject[i]["FaceBoundingBoxInInfraredSpace"]["Left"].asDouble();
+                face.faceBoundingBoxInInfraredSpace.top    = jsonObject[i]["FaceBoundingBoxInInfraredSpace"]["Top"].asDouble();
+                face.faceBoundingBoxInInfraredSpace.right  = jsonObject[i]["FaceBoundingBoxInInfraredSpace"]["Right"].asDouble();
+                face.faceBoundingBoxInInfraredSpace.bottom = jsonObject[i]["FaceBoundingBoxInInfraredSpace"]["Bottom"].asDouble();
                 
-                face.FaceBoundingBoxInColorSpace.left   = jsonObject[i]["FaceBoundingBoxInColorSpace"]["Left"];
-                face.FaceBoundingBoxInColorSpace.top    = jsonObject[i]["FaceBoundingBoxInColorSpace"]["Top"];
-                face.FaceBoundingBoxInColorSpace.right  = jsonObject[i]["FaceBoundingBoxInColorSpace"]["Right"];
-                face.FaceBoundingBoxInColorSpace.bottom = jsonObject[i]["FaceBoundingBoxInColorSpace"]["Bottom"];
+                face.FaceBoundingBoxInColorSpace.left   = jsonObject[i]["FaceBoundingBoxInColorSpace"]["Left"].asDouble();
+                face.FaceBoundingBoxInColorSpace.top    = jsonObject[i]["FaceBoundingBoxInColorSpace"]["Top"].asDouble();
+                face.FaceBoundingBoxInColorSpace.right  = jsonObject[i]["FaceBoundingBoxInColorSpace"]["Right"].asDouble();
+                face.FaceBoundingBoxInColorSpace.bottom = jsonObject[i]["FaceBoundingBoxInColorSpace"]["Bottom"].asDouble();
                 
-                face.faceRotationQuaternion.X = jsonObject[i]["FaceRotationQuaternion"]["X"];
-                face.faceRotationQuaternion.Y = jsonObject[i]["FaceRotationQuaternion"]["Y"];
-                face.faceRotationQuaternion.Z = jsonObject[i]["FaceRotationQuaternion"]["Z"];
-                face.faceRotationQuaternion.W = jsonObject[i]["FaceRotationQuaternion"]["W"];
+                face.faceRotationQuaternion.X = jsonObject[i]["FaceRotationQuaternion"]["X"].asDouble();
+                face.faceRotationQuaternion.Y = jsonObject[i]["FaceRotationQuaternion"]["Y"].asDouble();
+                face.faceRotationQuaternion.Z = jsonObject[i]["FaceRotationQuaternion"]["Z"].asDouble();
+                face.faceRotationQuaternion.W = jsonObject[i]["FaceRotationQuaternion"]["W"].asDouble();
                 
-                face.trackingId = jsonObject[i]["TrackingId"];
-                face.faceFrameFeatures = jsonObject[i]["FaceFrameFeatures"];
+                face.trackingId = jsonObject[i]["TrackingId"].asInt();
+                face.faceFrameFeatures = jsonObject[i]["FaceFrameFeatures"].asInt();
                 
                 faceArray.faces.push_back(face);
             }
