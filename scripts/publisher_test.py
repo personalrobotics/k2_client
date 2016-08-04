453b71a6e4a4f63f2d224d2334bc4d16cdb83af5
img14 <i.pi@comcast.net>
2016-08-04 11:34:43 -0400

    updating paths and dependencies

--- scripts/publisher_test.py
+++ scripts/publisher_test.py
@@ -4,8 +4,8 @@ from Person import Person
 from seat import Seat
 from k2_client.msg import PersonMessage
 from k2_client.msg import PersonArray
-from k2_client.msg import GameEvent
-from k2_client.msg import RobotBehavior
+from building_trust_nao_controller.msg import GameEvent
+from building_trust_nao_controller.msg import RobotBehavior
 import csv
 import time
 from time import gmtime, strftime
@@ -28,7 +28,7 @@ def talker():
 		game_event.properties = '{"Outcome":0,"Mistakers":["Person1","Person2"]}'
 	elif type_info == 4:
 		game_event.gameEvent = 2
-		game_event.properties = '{"Admitter":"Person"}'
+		game_event.properties = '{"Admitter":["People"]}'
 	elif type_info == 5:
 		game_event.gameEvent = 3
 		game_event.properties = '{"People":["Person1","Person2"]}'
